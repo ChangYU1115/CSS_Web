a45e35365e2c9cd34a234b57d653974e1fe4d7d3
ChangYu <changyu_wu@colerate.com.tw>
2024-05-09 17:05:35 +0800

    #122

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,19 @@
-import {styled} from 'styled-components';
+export default function Input({ label, inVaild, ...props }) {
+  let labelClasses = 'block mb-2 text-xs font-bold tracking-wide uppercase';
+  let inputClasses = 'w-full px-3 py-2 leading-tight border rounded shadow';
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$inVaild})=> $inVaild ? "#f87171" : "#6b7280"};
-`
+  if (inVaild){
+    labelClasses += ' text-red-400';
+    inputClasses += ' text-red-500 bg-red-100 border-red-300';
+  }else{
+    labelClasses += ' text-stone-300';
+    inputClasses += ' text-gray-700 bg-stone-300';
+  }
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$inVaild}) => $inVaild ? "#fed2d2" : "#d1d5db"} ;
-  color:${({$inVaild})=> $inVaild ? "#ef4444" : "#374151"} ;
-  border-color: ${({$inVaild})=> $inVaild ? "#f73f3f" : ""} ;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-
-export default function CustomInput({ label, inVaild, ...props}){
-    return(
-        <p>
-            <Label $inVaild={inVaild}>{label}</Label>
-            <Input $inVaild={inVaild} {...props} />
-        </p>
-    )
+  return (
+    <p>
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
+    </p>
+  );
 }
